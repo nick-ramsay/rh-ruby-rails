d9cceb5e7e5443cd27f1a669690be12132052729
Nick Ramsay <NickRamsay1989@gmail.com>
2020-02-18 21:28:19 +1100

    Order card now rendering for each API order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,21 @@
 //= require jquery
 
-function testFunction() {
-  var testData;
+function renderData() {
+  var orderData;
   $.ajax({
     url: "/orders",
     type: "get",
     dataType: "json",
-    success: function (data) { testData = data }
+    success: function (data) { orderData = data }
   }).then(function () {
-    console.log(testData);
+    console.log(orderData);
+    for (i = 0; i < orderData.length; i++) {
+    $(".order-list").append('<div class="order-container" id="order-container' + i +'">');
+    $("#order-container" + i).append('<div class="order-title-container"><h5 class="order-title">Agency Name - Campaign, Address, Suburb</h5></div>');
+    $("#order-container" + i).append('<div class="order-details-container" id="order-details-container' + i +'"></div>');
+    $("#order-details-container" + i).append('<div class="order-item-name"><span>Order Item Name</span></div>');
+    $("#order-details-container" + i).append('<div class="order-item-functions" id="order-item-functions'+ i +'"></div>');
+    $("#order-item-functions" + i).append('<span class="order-download-link">Download Artwork</span><span class="order-line-break"> | </span><span class="order-status-change">Change Status</span><span class="order-line-break"> | </span><span class="order-status">Current Order Status</span>');
+  }
   })
 }
